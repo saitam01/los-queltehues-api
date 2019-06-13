01d032d305f95d68b228202ec7e6021e108eac07
matias aguirre <matias.aguirre@globant.com>
2019-06-12 21:13:07 -0400

    CORS

--- src/main/java/cl/queltehues/api/controller/SecurityController.java
+++ src/main/java/cl/queltehues/api/controller/SecurityController.java
@@ -5,6 +5,7 @@ import cl.queltehues.api.service.SecurityService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,6 +30,7 @@ public class SecurityController {
             notes = "Validate user and password",
             response = Map.class
     )
+    @CrossOrigin(origins = "http://localhost:4200")
     public Collection authenticateUser(@RequestParam(value = "jwt") String token) throws DriveException {
         return securityService.validateUser(token);
     }
